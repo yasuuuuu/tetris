7f16d4e135c5ad6ac4dd1d0a31bcc247e8522e48
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2017-01-23 01:52:51 +0900

    arrange dir structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,17 +4,17 @@ import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
 
 gulp.task('babel', () => {
-  gulp.src('./es6/*.es6.js')
+  gulp.src('./javascript/es6/*.es6.js')
     .pipe(plumber())
     .pipe(babel())
     .pipe(rename((path) => {
       path.basename = path.basename.replace(/.es6$/g, '');
     }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./javascript/'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./es6/*.es6.js', ['babel']);
+  gulp.watch('./javascript/es6/*.es6.js', ['babel']);
 });
 
 gulp.task('default', ['babel', 'watch']);
