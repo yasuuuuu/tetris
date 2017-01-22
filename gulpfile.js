8744b20cc074c1e71cdd50c9e23a4c3cf30a5262
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2017-01-23 00:42:57 +0900

    gulp便利杉問題

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp  = require("gulp");
-var babel = require("gulp-babel");
-var rename = require("gulp-rename");
+var gulp  = require('gulp');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 
-gulp.task("babel_build", function () {
-  return gulp.src("src/*es6.js")
+gulp.task('babel', function () {
+  gulp.src('./es6/*.es6.js')
+    .pipe(plumber())
     .pipe(babel())
-    // ".es6"という文字列を削る
-    // path.basename -> sample.es6
     .pipe(rename(function (path) {
       var cutLength = path.basename.length - 4;
       path.basename = path.basename.slice(0, cutLength);
     }))
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('./'));
 });
 
-gulp.task('babel_watch', function() {
-  gulp.watch('src/*es6.js', ['babel_build'])
+gulp.task('watch', function() {
+  gulp.watch('./es6/*.es6.js', ['babel'])
 });
+
+gulp.task('default', ['babel', 'watch']);
